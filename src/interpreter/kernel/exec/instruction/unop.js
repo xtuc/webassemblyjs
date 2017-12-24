7732abddda682f856eb187c3b5f359cc53df2e21
Mauro Bringolf <mauro@webkinder.ch>
2017-12-24 11:01:23 +0100

    Redo f32.fabs on new structure

---
+++ src/interpreter/kernel/exec/instruction/unop.js
@@ -0,0 +1,39 @@
+// @flow
+
+type Sign = 'abs';
+
+const i32 = require('../../../runtime/values/i32');
+const i64 = require('../../../runtime/values/i64');
+const f32 = require('../../../runtime/values/f32');
+const f64 = require('../../../runtime/values/f64');
+
+// https://webassembly.github.io/spec/exec/instructions.html#exec-binop
+function unop(
+  c: StackLocal,
+  sign: Sign,
+  createValue: (number) => StackLocal
+): StackLocal {
+
+  switch (sign) {
+  case 'abs':
+    return createValue(Math.abs(c.value));
+  }
+
+  throw new Error('Unsupported unop: ' + sign);
+}
+
+export function unopi32(c: StackLocal, sign: Sign): StackLocal {
+  return unop(c, sign, i32.createValue);
+}
+
+export function unopi64(c: StackLocal, sign: Sign): StackLocal {
+  return unop(c, sign, i64.createValue);
+}
+
+export function unopf32(c: StackLocal, sign: Sign): StackLocal {
+  return unop(c, sign, f32.createValue);
+}
+
+export function unopf64(c: StackLocal, sign: Sign): StackLocal {
+  return unop(c, sign, f64.createValue);
+}
---
+++ test/interpreter/fixtures/fabs/module.wast
@@ -0,0 +1,7 @@
+(module
+  (func $abs (param f32) (result f32) (
+    (get_local 0)
+    (f32.abs)
+  ))
+ (export "abs" (func $abs))
+)
