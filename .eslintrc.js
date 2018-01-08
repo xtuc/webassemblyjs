7047fccf93cc07c42c625de2d12e58034f40796e
Sven SAULEAU <sven.sauleau@xtuc.fr>
2018-01-08 15:07:45 +0100

    fix: prettier configuration in eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,32 +3,27 @@
 module.exports = {
   parser: 'babel-eslint',
   extends: [
+    'prettier',
     'eslint:recommended',
     'plugin:flowtype/recommended'
   ],
   plugins: [
     'flowtype',
     'mocha',
-    'flowtype-errors'
+    'flowtype-errors',
+    'prettier'
   ],
   rules: {
     "mocha/no-exclusive-tests": "error",
     "mocha/no-identical-title": "error",
     "mocha/no-skipped-tests": "warn",
     'flowtype-errors/show-errors': 2,
-    'array-bracket-spacing': ['error', 'never'],
-    'object-curly-spacing': ['error', 'never'],
-    'arrow-parens': ['error', 'always'],
-    'arrow-spacing': ['error', {before: true, after: true}],
     camelcase: 'off',
     'comma-dangle': 'off',
     'consistent-return': 'off',
     curly: 'off',
-    indent: ['error', 2],
     'linebreak-style': ['error', 'unix'],
     'key-spacing': 'off',
-    'keyword-spacing': 'error',
-    'max-len': ['error', 110, 2],
     'no-case-declarations': 'off',
     'no-cond-assign': 'off',
     'no-console': 'off',
@@ -42,16 +37,10 @@ module.exports = {
     'no-process-exit': 'off',
     'no-return-assign': 'off',
     'no-shadow': 'off',
-    'no-trailing-spaces': 'error',
-    'no-underscore-dangle': 'off',
     'no-unreachable': 'off',
     'no-use-before-define': 'off',
     'no-var': 'error',
     'prefer-const': 'error',
-    quotes: ['error', 'single', {allowTemplateLiterals: true}],
-    'space-before-blocks': ['error', 'always'],
-    'space-infix-ops': 'error',
-    semi: ['error', 'always'],
     strict: 'off'
   },
   parserOptions: {
