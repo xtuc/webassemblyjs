c0f86c8e5f747d5378bef42471e7bc66d78116f7
Sven SAULEAU <sven.sauleau@xtuc.fr>
2018-02-02 20:09:19 +0100

    feat(repl): implicit module

--- src/interpreter/runtime/values/global.js
+++ src/interpreter/runtime/values/global.js
@@ -9,23 +9,21 @@ export function createInstance(allocator: Allocator, node: Global) {
   let value;
   const { valtype, mutability } = node.globalType;
 
-  if (node.init.length > 0) {
-    if (isConst(node.init) === false) {
-      throw new CompileError("constant expression required");
-    }
+  if (isConst(node.init) === false) {
+    throw new CompileError("constant expression required");
+  }
 
-    // Validate the type
-    const resultInferedType = getType(node.init);
+  // Validate the type
+  const resultInferedType = getType(node.init);
 
-    if (node.globalType.valtype !== resultInferedType) {
-      throw new CompileError("type mismatch");
-    }
+  if (node.globalType.valtype !== resultInferedType) {
+    throw new CompileError("type mismatch");
+  }
 
-    const res = evaluate(allocator, node.init);
+  const res = evaluate(allocator, node.init);
 
-    if (res != null) {
-      value = res.value;
-    }
+  if (res != null) {
+    value = res.value;
   }
 
   return {
