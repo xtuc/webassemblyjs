891494e9215e84af74e8b0aa48cd7512933faa78
Sven SAULEAU <sven.sauleau@xtuc.fr>
2018-02-09 16:51:42 +0100

    fix: type eq

--- src/compiler/validation/func-result-type.js
+++ src/compiler/validation/func-result-type.js
@@ -3,6 +3,20 @@
 const { traverse } = require("../AST/traverse");
 const { getType } = require("./type-inference");
 
+function typeEq(l: Array<Valtype>, r: Array<Valtype>): boolean {
+  if (l.length !== r.length) {
+    return false;
+  }
+
+  for (let i = 0; i < l.length; i++) {
+    if (l[i] != r[i]) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
 export default function validate(ast: Program): Array<string> {
   const errors = [];
 
@@ -10,21 +24,22 @@ export default function validate(ast: Program): Array<string> {
     Func({ node }: NodePath<Func>) {
       // Since only one return is allowed at the moment, we don't need to check
       // them all.
-      const [resultType] = node.result;
+      const resultType = node.result;
 
       // Function has no result types or last instruction, we can skip it
-      if (typeof resultType !== "string") {
+      if (resultType.length === 0) {
         return;
       }
 
-      const lastInstructionResultType = getType(node.body);
+      const inferedResultType = getType(node.body);
 
       // Type is unknown, we can not verify the result type
-      if (!lastInstructionResultType) {
+      if (typeof inferedResultType === "undefined") {
         return;
       }
 
-      if (resultType != lastInstructionResultType) {
+      // $FlowIgnore
+      if (typeEq(resultType, inferedResultType) === false) {
         let name = "anonymous";
 
         if (node.name != null) {
@@ -32,8 +47,9 @@ export default function validate(ast: Program): Array<string> {
         }
 
         errors.push(
-          `- function '${name}' expected result type ${resultType},` +
-            ` but ${lastInstructionResultType} given.`
+          `- function '${name}' expected result type ${JSON.stringify(
+            resultType
+          )},` + ` but ${JSON.stringify(inferedResultType)} given.`
         );
 
         return;
--- src/compiler/validation/type-inference.js
+++ src/compiler/validation/type-inference.js
@@ -2,7 +2,7 @@
 
 const { signatures } = require("../AST/signatures");
 
-export function getType(instrs: Array<Instruction>): ?string {
+export function getType(instrs: Array<Instruction>): ?Array<Valtype> {
   if (instrs.length === 0) {
     return;
   }
@@ -22,7 +22,7 @@ export function getType(instrs: Array<Instruction>): ?string {
   }
 
   // Can't infer it, need to interpreter it
-  if (last.id === "get_global") {
+  if (last.id === "get_global" || last.id === "get_local") {
     return;
   }
 
@@ -31,7 +31,16 @@ export function getType(instrs: Array<Instruction>): ?string {
     const loop: LoopInstruction = last;
 
     if (loop.resulttype != null) {
-      return loop.resulttype;
+      return [loop.resulttype];
+    }
+  }
+
+  if (last.id === "If") {
+    // $FlowIgnore: if id is `loop` we can assume it's a LoopInstruction
+    const ifInstruction: IfInstruction = last;
+
+    if (ifInstruction.result != null) {
+      return [ifInstruction.result];
     }
   }
 }
