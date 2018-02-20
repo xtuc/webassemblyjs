569f5ac014e1b137662f92d482a771fedb0bea87
Sven SAULEAU <sven.sauleau@xtuc.fr>
2018-02-20 09:20:47 +0100

    Deploy website
    Deploy website version based on 76d44813328b5e61af218a18a85917900246e254

--- js/share.js
+++ js/share.js
@@ -1,16 +1,63 @@
+function runWasmgen(buffer) {
+  const output = document.getElementById("wasmgen-output");
+  const editor = document.getElementById("wasmgen-editor");
+
+  const opts = {
+    out: "text"
+  };
+
+  const out = wasmgen(buffer, opts);
+
+  const lineCount = out.split("\n").length - 1;
+  const sizePerLine = 20;
+
+  editor.style.height = lineCount * sizePerLine;
+
+  output.innerHTML = '<code className="language-txt">' + out + "</code>";
+}
+
+function runWasm2Wast(buffer) {
+  const output = document.getElementById("wasm2wast-output");
+  const editor = document.getElementById("wasm2wast-editor");
+
+  function showOutput(out) {
+    const lineCount = out.split("\n").length - 1;
+    const sizePerLine = 20;
+
+    editor.style.height = lineCount * sizePerLine;
+
+    output.innerHTML = '<code className="language-wast">' + out + "</code>";
+  }
+
+  function toWast(ast) {
+    return webassemblyInterpreter.printers.printWAST(ast);
+  }
+
+  function parseBinary(buff) {
+    try {
+      return webassemblyInterpreter.parsers.parseWASM(buff);
+    } catch (e) {
+      showOutput("Error: " + e.message);
+      throw e;
+    }
+  }
+
+  const ast = parseBinary(buffer);
+  const wastOut = toWast(ast);
+
+  showOutput(wastOut);
+}
+
 (function() {
-  const input = document.getElementById("upload");
-  const output = document.getElementById("output");
-  const editor = document.getElementById("editor");
   const downloadBtn = document.getElementById("download");
+  const input = document.getElementById("upload");
 
   if (typeof webassemblyInterpreter === "undefined") {
     throw new Error("webassemblyInterpreter has not been loaded");
   }
 
   function downloadURL(data, fileName) {
-    let a;
-    a = document.createElement("a");
+    const a = document.createElement("a");
     a.href = data;
     a.download = fileName;
     document.body.appendChild(a);
@@ -35,47 +82,6 @@
     return buff.buffer;
   }
 
-  function showOutput(out) {
-    const lineCount = out.split("\n").length;
-    const sizePerLine = 20;
-
-    editor.style.height = lineCount * sizePerLine;
-
-    output.innerHTML = '<code className="language-wast">' + out + "</code>";
-  }
-
-  function toWast(ast) {
-    return webassemblyInterpreter.printers.printWAST(ast);
-  }
-
-  function parseBinary(buff) {
-    try {
-      return webassemblyInterpreter.parsers.parseWASM(buff);
-    } catch (e) {
-      showOutput("Error: " + e.message);
-      throw e;
-    }
-  }
-
-  input.addEventListener("change", function(event) {
-    const file = event.target.files[0];
-
-    const fileReader = new FileReader();
-
-    fileReader.onload = function(e) {
-      const arrayBuffer = e.target.result;
-
-      encodeInUrl(arrayBuffer);
-
-      const ast = parseBinary(arrayBuffer);
-      const wastOut = toWast(ast);
-
-      showOutput(wastOut);
-    };
-
-    fileReader.readAsArrayBuffer(file);
-  });
-
   downloadBtn.addEventListener("click", function() {
     const savedBuffer = decodeFromUrl();
 
@@ -93,12 +99,27 @@
     window.URL.revokeObjectURL(objectUrl);
   });
 
+  input.addEventListener("change", function(event) {
+    const file = event.target.files[0];
+
+    const fileReader = new FileReader();
+
+    fileReader.onload = function(e) {
+      const arrayBuffer = e.target.result;
+
+      encodeInUrl(arrayBuffer);
+
+      runWasm2Wast(arrayBuffer);
+      runWasmgen(arrayBuffer);
+    };
+
+    fileReader.readAsArrayBuffer(file);
+  });
+
   const savedBuffer = decodeFromUrl();
 
   if (typeof savedBuffer !== "undefined") {
-    const ast = parseBinary(savedBuffer);
-    const wastOut = toWast(ast);
-
-    showOutput(wastOut);
+    runWasm2Wast(savedBuffer);
+    runWasmgen(savedBuffer);
   }
 })();
