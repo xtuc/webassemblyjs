043e465176a63381f047a48d206ebe61a7e32ffb
Sven SAULEAU <sven.sauleau@xtuc.fr>
2018-02-20 09:19:52 +0100

    feat(website): add wasmgen on share page

--- website/pages/en/share.js
+++ website/pages/en/share.js
@@ -32,13 +32,25 @@ class Share extends React.Component {
           <div className="window-header">
             <div className="action-buttons" />
 
-            <span className="language">wasm2wast</span>
+            <span className="language"><code>wasmgen -o text</code></span>
           </div>
-          <div className="window-body" id="editor">
-            <pre className="code-output" id="output"></pre>
+          <div className="window-body" id="wasmgen-editor">
+            <pre className="code-output" id="wasmgen-output"></pre>
           </div>
         </div>
 
+        <div className="window" style={{ width: "80%" }}>
+          <div className="window-header">
+            <div className="action-buttons" />
+
+            <span className="language"><code>wasm2wast</code></span>
+          </div>
+          <div className="window-body" id="wasm2wast-editor">
+            <pre className="code-output" id="wasm2wast-output"></pre>
+          </div>
+        </div>
+
+        <script src="https://bundle.run/wasmgen@1.4.1" />
         <script src="https://bundle.run/webassembly-interpreter@0.0.29/lib/tools.js" />
         <script src="/js/share.js" />
 
