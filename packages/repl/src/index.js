2f2187bdf2bb943fce3851fae76f40cad695ed2d
Sven SAULEAU <sven.sauleau@xtuc.fr>
2018-04-06 17:08:21 +0200

    fix: fix linting

--- packages/repl/src/index.js
+++ packages/repl/src/index.js
@@ -3,7 +3,6 @@ const {
 } = require("webassemblyjs/lib/compiler/compile/module");
 const { Instance } = require("webassemblyjs/lib/interpreter");
 const partialEvaluation = require("webassemblyjs/lib/interpreter/partial-evaluation");
-const t = require("@webassemblyjs/ast");
 const { parse } = require("@webassemblyjs/wast-parser");
 const {
   Memory
