f61d5b5d4844a1648a1412dcb12671e66ec40874
Sven SAULEAU <sven.sauleau@xtuc.fr>
2018-04-26 18:40:19 +0200

    refactor(dce): use test framework

--- packages/dce/test/index.js
+++ packages/dce/test/index.js
@@ -1,49 +1,31 @@
+// @flow
+
+const {
+  getFixtures,
+  compareWithExpected
+} = require("@webassemblyjs/helper-test-framework");
+
 const { decode } = require("@webassemblyjs/wasm-parser");
 const { print } = require("@webassemblyjs/wast-printer");
 
-const diff = require("jest-diff");
-const { NO_DIFF_MESSAGE } = require("jest-diff/build/constants");
-const chai = require("chai");
-const glob = require("glob");
-const { readFileSync, writeFileSync } = require("fs");
-const path = require("path");
+const { readFileSync } = require("fs");
+const { dirname, join } = require("path");
 
 const loader = require("../src/index");
 const getUsedExports = require("../src/used-exports");
 
 describe("Eliminate unused", () => {
-  const testSuites = glob.sync("packages/dce/test/fixtures/**/actual.wast");
+  const testSuites = getFixtures(__dirname, "fixtures", "**/actual.wast");
 
-  testSuites.forEach(suite => {
-    it(suite, () => {
-      const wastModule = readFileSync(suite, "utf8");
+  const pre = (f, suite) => {
+    const userFile = join(dirname(suite), "user.tjs");
 
-      const userFile = path.join(path.dirname(suite), "user.tjs");
-      const expectedFile = path.join(path.dirname(suite), "expected.wast");
+    const usedExports = getUsedExports(readFileSync(userFile, "utf8"));
 
-      const usedExports = getUsedExports(readFileSync(userFile, "utf8"));
+    const actualBuff = loader(f, usedExports);
 
-      const actualBuff = loader(wastModule, usedExports);
-      const actualWast = print(decode(actualBuff));
+    return print(decode(actualBuff));
+  };
 
-      let expected;
-      try {
-        expected = readFileSync(expectedFile, "utf8");
-      } catch (e) {
-        expected = actualWast;
-
-        writeFileSync(expectedFile, actualWast);
-
-        console.log("Write expected file", expectedFile);
-      }
-
-      const out = diff(actualWast.trim(), expected.trim());
-
-      if (out !== null && out !== NO_DIFF_MESSAGE) {
-        throw new Error("\n" + out);
-      }
-
-      chai.expect(actualWast).to.be.equal(expected);
-    });
-  });
+  compareWithExpected(testSuites, pre, "expected.wast");
 });
