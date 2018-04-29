64c4ab359a483c3c0c06088b1164332987e9a778
Sven SAULEAU <sven.sauleau@xtuc.fr>
2018-04-29 21:54:49 +0200

    fix: improve buffer comparison a bit

--- packages/helper-buffer/src/compare.js
+++ packages/helper-buffer/src/compare.js
@@ -3,16 +3,23 @@ const diff = require("jest-diff");
 const { NO_DIFF_MESSAGE } = require("jest-diff/build/constants");
 const { decode } = require("@webassemblyjs/wasm-parser");
 
-export function compareArrayBuffers(l, r) {
-  const oldConsoleLog = console.log;
+const oldConsoleLog = console.log;
 
+export function compareArrayBuffers(l, r) {
   /**
    * Decode left
    */
   let bufferL = "";
 
   console.log = (...texts) => (bufferL += texts.join("") + "\n");
-  decode(l, { dump: true });
+
+  try {
+    decode(l, { dump: true });
+  } catch (e) {
+    console.error(bufferL);
+    console.error(e);
+    throw e;
+  }
 
   /**
    * Decode right
@@ -20,7 +27,14 @@ export function compareArrayBuffers(l, r) {
   let bufferR = "";
 
   console.log = (...texts) => (bufferR += texts.join("") + "\n");
-  decode(r, { dump: true });
+
+  try {
+    decode(r, { dump: true });
+  } catch (e) {
+    console.error(bufferR);
+    console.error(e);
+    throw e;
+  }
 
   console.log = oldConsoleLog;
 
