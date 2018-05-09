7fd15a2a145b01a20153d4f7985de3340c2b910d
Sven SAULEAU <sven.sauleau@xtuc.fr>
2018-05-09 09:58:19 +0200

    chore: [skip ci] bump webassemblyjs on website

--- website/pages/en/share.js
+++ website/pages/en/share.js
@@ -50,9 +50,9 @@ class Share extends React.Component {
           </div>
         </div>
 
-        <script src="https://bundle.run/@webassemblyjs/wasm-gen@1.2.6" />
-        <script src="https://bundle.run/@webassemblyjs/wasm-parser@1.2.6" />
-        <script src="https://bundle.run/@webassemblyjs/wast-printer@1.2.6" />
+        <script src="https://bundle.run/@webassemblyjs/wasm-gen@1.3.3" />
+        <script src="https://bundle.run/@webassemblyjs/wasm-parser@1.3.3" />
+        <script src="https://bundle.run/@webassemblyjs/wast-printer@1.3.3" />
         <script src="/js/share.js" />
 
         <p style={{textAlign: 'center'}}>
