5f5d8cfcda74e341cea258590e8619e87c15b576
Sven SAULEAU <sven.sauleau@xtuc.fr>
2018-05-09 09:01:41 +0200

    fix: move building fuzzer in build phase

--- scripts/test.sh
+++ scripts/test.sh
@@ -7,14 +7,6 @@ set -e
 PACKAGES="./packages/*"
 
 
-## Launch pretest scripts if provided
-# for D in ./packages/*; do
-#     (npm run pretest --silent --prefix $D || true) &
-#     echo "launch pretest target for $D ..."
-# done
-
-wait
-
 ./node_modules/.bin/mocha "$PACKAGES/test/**/*.js" \
     --recursive \
     --reporter=tap \
