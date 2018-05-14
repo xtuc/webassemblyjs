3c25cdc1df2853f930da914ade76517555e6ec02
Sven SAULEAU <sven.sauleau@xtuc.fr>
2018-05-14 12:04:22 +0200

    Revert "feat: debug"
    This reverts commit b5ea8cf786e162c93bd596667601e31aed1dd2fe.

--- scripts/generate-ast-utils.sh
+++ scripts/generate-ast-utils.sh
@@ -4,5 +4,3 @@ pkg=packages/ast
 
 node $pkg/scripts/generateTypeDefinitions.js | npx prettier > $pkg/src/types/nodes.js
 node $pkg/scripts/generateNodeUtils.js | npx prettier > $pkg/src/nodes.js
-
-cat -e $pkg/src/nodes.js
