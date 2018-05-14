e8e5e4881e24f3143f7fefd4f3fdf9f3a187b82b
Sven SAULEAU <sven.sauleau@xtuc.fr>
2018-05-14 11:20:02 +0200

    feat: add assert/test

--- packages/ast/scripts/generate.sh
+++ packages/ast/scripts/generate.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 node ./scripts/generateTypeDefinitions.js | npx prettier > src/types.js
-node ./scripts/generateConstructors.js | npx prettier > src/nodes.js
+node ./scripts/generateNodeUtils.js | npx prettier > src/nodes.js
