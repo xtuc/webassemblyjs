ecdc60f0efc668ee61a9a8c5f3b9abb455976c99
Sven SAULEAU <sven.sauleau@xtuc.fr>
2018-05-14 11:57:40 +0200

    feat: try with flow parser

--- scripts/generate-ast-utils.sh
+++ scripts/generate-ast-utils.sh
@@ -2,5 +2,5 @@
 
 pkg=packages/ast
 
-node $pkg/scripts/generateTypeDefinitions.js | npx prettier > $pkg/src/types/nodes.js
-node $pkg/scripts/generateNodeUtils.js | npx prettier > $pkg/src/nodes.js
+node $pkg/scripts/generateTypeDefinitions.js | npx prettier --parser flow > $pkg/src/types/nodes.js
+node $pkg/scripts/generateNodeUtils.js | npx prettier --parser flow > $pkg/src/nodes.js
