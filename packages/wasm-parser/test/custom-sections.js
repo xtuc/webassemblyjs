5bdcde9992be66ba944b37e22b9275b4c88e82df
Tim Hambourger <tim.hambourger@gmail.com>
2018-07-21 22:46:12 -0500

    wasm-parser -- Make AST assertions in custom sections tests more focused

--- packages/wasm-parser/test/custom-sections.js
+++ packages/wasm-parser/test/custom-sections.js
@@ -1,3 +1,4 @@
+const { assert } = require("chai");
 const {
   encodeVersion,
   encodeHeader,
@@ -7,7 +8,7 @@ const {
 const { makeBuffer } = require("@webassemblyjs/helper-buffer");
 const constants = require("@webassemblyjs/helper-wasm-bytecode").default;
 
-const { compareStrings } = require("@webassemblyjs/helper-test-framework");
+const { traverse } = require("@webassemblyjs/ast");
 
 const { decode } = require("../lib");
 
@@ -28,12 +29,21 @@ describe("Binary decoder", () => {
       randCustSection("cust4", 4)
     );
 
+    // Step 1. Just make sure we can decode the input w/o throwing.
+    // That hasn't always been the case for this input.
     const ast = decode(bin);
-    const actual = JSON.stringify(ast, null, 2);
 
-    const expected = JSON.stringify(expectedForInterspersedCustom(), null, 2);
-
-    compareStrings(actual, expected);
+    // Step 2. As extra sanity check, make sure output AST has the expected sections.
+    const sections = findSections(ast);
+    assert.deepEqual(sections, [
+      "custom",
+      "type",
+      "custom",
+      "func",
+      "custom",
+      "code",
+      "custom"
+    ]);
   });
 
   it("should decode a binary with another custom section after the custom name section", () => {
@@ -64,12 +74,13 @@ describe("Binary decoder", () => {
       randCustSection("?", 5)
     );
 
+    // Step 1. Just make sure we can decode the input w/o throwing.
+    // That hasn't always been the case for this input.
     const ast = decode(bin);
-    const actual = JSON.stringify(ast, null, 2);
 
-    const expected = JSON.stringify(expectedForCustomAfterName(), null, 2);
-
-    compareStrings(actual, expected);
+    // Step 2. As extra sanity check, make sure output AST has the expected sections.
+    const sections = findSections(ast);
+    assert.deepEqual(sections, ["type", "func", "code", "custom", "custom"]);
   });
 });
 
@@ -91,466 +102,12 @@ function randCustSection(name, contentLength) {
   ];
 }
 
-function expectedForInterspersedCustom() {
-  return {
-    type: "Program",
-    body: [
-      {
-        type: "Module",
-        id: null,
-        fields: [
-          {
-            type: "TypeInstruction",
-            functype: {
-              type: "Signature",
-              params: [],
-              results: []
-            },
-            loc: {
-              start: {
-                line: -1,
-                column: 20
-              },
-              end: {
-                line: -1,
-                column: 23
-              }
-            }
-          },
-          {
-            type: "Func",
-            name: {
-              type: "Identifier",
-              value: "func_0",
-              raw: ""
-            },
-            signature: {
-              type: "Signature",
-              params: [],
-              results: []
-            },
-            body: [],
-            loc: {
-              start: {
-                line: -1,
-                column: 198
-              },
-              end: {
-                line: -1,
-                column: 201
-              }
-            },
-            metadata: {
-              bodySize: 2
-            }
-          }
-        ],
-        metadata: {
-          type: "ModuleMetadata",
-          sections: [
-            {
-              type: "SectionMetadata",
-              section: "custom",
-              startOffset: 9,
-              size: {
-                type: "NumberLiteral",
-                value: 7,
-                raw: "7",
-                loc: {
-                  start: {
-                    line: -1,
-                    column: 9
-                  },
-                  end: {
-                    line: -1,
-                    column: 10
-                  }
-                }
-              }
-            },
-            {
-              type: "SectionMetadata",
-              section: "type",
-              startOffset: 18,
-              size: {
-                type: "NumberLiteral",
-                value: 4,
-                raw: "4",
-                loc: {
-                  start: {
-                    line: -1,
-                    column: 18
-                  },
-                  end: {
-                    line: -1,
-                    column: 19
-                  }
-                }
-              },
-              vectorOfSize: {
-                type: "NumberLiteral",
-                value: 1,
-                raw: "1",
-                loc: {
-                  start: {
-                    line: -1,
-                    column: 19
-                  },
-                  end: {
-                    line: -1,
-                    column: 20
-                  }
-                }
-              }
-            },
-            {
-              type: "SectionMetadata",
-              section: "custom",
-              startOffset: 24,
-              size: {
-                type: "NumberLiteral",
-                value: 8,
-                raw: "8",
-                loc: {
-                  start: {
-                    line: -1,
-                    column: 24
-                  },
-                  end: {
-                    line: -1,
-                    column: 25
-                  }
-                }
-              }
-            },
-            {
-              type: "SectionMetadata",
-              section: "func",
-              startOffset: 34,
-              size: {
-                type: "NumberLiteral",
-                value: 2,
-                raw: "2",
-                loc: {
-                  start: {
-                    line: -1,
-                    column: 34
-                  },
-                  end: {
-                    line: -1,
-                    column: 35
-                  }
-                }
-              },
-              vectorOfSize: {
-                type: "NumberLiteral",
-                value: 1,
-                raw: "1",
-                loc: {
-                  start: {
-                    line: -1,
-                    column: 35
-                  },
-                  end: {
-                    line: -1,
-                    column: 36
-                  }
-                }
-              }
-            },
-            {
-              type: "SectionMetadata",
-              section: "custom",
-              startOffset: 38,
-              size: {
-                type: "NumberLiteral",
-                value: 155,
-                raw: "155",
-                loc: {
-                  start: {
-                    line: -1,
-                    column: 38
-                  },
-                  end: {
-                    line: -1,
-                    column: 40
-                  }
-                }
-              }
-            },
-            {
-              type: "SectionMetadata",
-              section: "code",
-              startOffset: 196,
-              size: {
-                type: "NumberLiteral",
-                value: 4,
-                raw: "4",
-                loc: {
-                  start: {
-                    line: -1,
-                    column: 196
-                  },
-                  end: {
-                    line: -1,
-                    column: 197
-                  }
-                }
-              },
-              vectorOfSize: {
-                type: "NumberLiteral",
-                value: 1,
-                raw: "1",
-                loc: {
-                  start: {
-                    line: -1,
-                    column: 197
-                  },
-                  end: {
-                    line: -1,
-                    column: 198
-                  }
-                }
-              }
-            },
-            {
-              type: "SectionMetadata",
-              section: "custom",
-              startOffset: 202,
-              size: {
-                type: "NumberLiteral",
-                value: 10,
-                raw: "10",
-                loc: {
-                  start: {
-                    line: -1,
-                    column: 202
-                  },
-                  end: {
-                    line: -1,
-                    column: 203
-                  }
-                }
-              }
-            }
-          ]
-        }
-      }
-    ]
-  };
-}
-
-function expectedForCustomAfterName() {
-  return {
-    type: "Program",
-    body: [
-      {
-        type: "Module",
-        id: null,
-        fields: [
-          {
-            type: "TypeInstruction",
-            functype: {
-              type: "Signature",
-              params: [],
-              results: []
-            },
-            loc: {
-              start: {
-                line: -1,
-                column: 11
-              },
-              end: {
-                line: -1,
-                column: 14
-              }
-            }
-          },
-          {
-            type: "Func",
-            name: {
-              type: "Identifier",
-              value: "foo"
-            },
-            signature: {
-              type: "Signature",
-              params: [],
-              results: []
-            },
-            body: [],
-            loc: {
-              start: {
-                line: -1,
-                column: 21
-              },
-              end: {
-                line: -1,
-                column: 24
-              }
-            },
-            metadata: {
-              bodySize: 2
-            }
-          }
-        ],
-        metadata: {
-          type: "ModuleMetadata",
-          sections: [
-            {
-              type: "SectionMetadata",
-              section: "type",
-              startOffset: 9,
-              size: {
-                type: "NumberLiteral",
-                value: 4,
-                raw: "4",
-                loc: {
-                  start: {
-                    line: -1,
-                    column: 9
-                  },
-                  end: {
-                    line: -1,
-                    column: 10
-                  }
-                }
-              },
-              vectorOfSize: {
-                type: "NumberLiteral",
-                value: 1,
-                raw: "1",
-                loc: {
-                  start: {
-                    line: -1,
-                    column: 10
-                  },
-                  end: {
-                    line: -1,
-                    column: 11
-                  }
-                }
-              }
-            },
-            {
-              type: "SectionMetadata",
-              section: "func",
-              startOffset: 15,
-              size: {
-                type: "NumberLiteral",
-                value: 2,
-                raw: "2",
-                loc: {
-                  start: {
-                    line: -1,
-                    column: 15
-                  },
-                  end: {
-                    line: -1,
-                    column: 16
-                  }
-                }
-              },
-              vectorOfSize: {
-                type: "NumberLiteral",
-                value: 1,
-                raw: "1",
-                loc: {
-                  start: {
-                    line: -1,
-                    column: 16
-                  },
-                  end: {
-                    line: -1,
-                    column: 17
-                  }
-                }
-              }
-            },
-            {
-              type: "SectionMetadata",
-              section: "code",
-              startOffset: 19,
-              size: {
-                type: "NumberLiteral",
-                value: 4,
-                raw: "4",
-                loc: {
-                  start: {
-                    line: -1,
-                    column: 19
-                  },
-                  end: {
-                    line: -1,
-                    column: 20
-                  }
-                }
-              },
-              vectorOfSize: {
-                type: "NumberLiteral",
-                value: 1,
-                raw: "1",
-                loc: {
-                  start: {
-                    line: -1,
-                    column: 20
-                  },
-                  end: {
-                    line: -1,
-                    column: 21
-                  }
-                }
-              }
-            },
-            {
-              type: "SectionMetadata",
-              section: "custom",
-              startOffset: 25,
-              size: {
-                type: "NumberLiteral",
-                value: 13,
-                raw: "13",
-                loc: {
-                  start: {
-                    line: -1,
-                    column: 25
-                  },
-                  end: {
-                    line: -1,
-                    column: 26
-                  }
-                }
-              }
-            },
-            {
-              type: "SectionMetadata",
-              section: "custom",
-              startOffset: 40,
-              size: {
-                type: "NumberLiteral",
-                value: 7,
-                raw: "7",
-                loc: {
-                  start: {
-                    line: -1,
-                    column: 40
-                  },
-                  end: {
-                    line: -1,
-                    column: 41
-                  }
-                }
-              }
-            }
-          ],
-          functionNames: [
-            {
-              type: "FunctionNameMetadata",
-              value: "foo",
-              index: 0
-            }
-          ]
-        }
-      }
-    ]
-  };
+function findSections(ast) {
+  const sections = [];
+  traverse(ast, {
+    SectionMetadata({ node }) {
+      sections.push(node.section);
+    }
+  });
+  return sections;
 }
