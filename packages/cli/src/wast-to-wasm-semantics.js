d2852071537045fa593b2efa774ba6ced1ec2a21
Sven SAULEAU <sven.sauleau@xtuc.fr>
2018-08-15 20:32:31 +0200

    feat(cli): add wast-to-wasm-semantics

---
+++ packages/cli/src/wast-to-wasm-semantics.js
@@ -0,0 +1,23 @@
+#!/usr/bin/env node
+// @flow
+
+
+const wastIdentifierToIndex = require("@webassemblyjs/ast/lib/transform/wast-identifier-to-index");
+const denormalizeTypeReferences = require("@webassemblyjs/ast/lib/transform/denormalize-type-references");
+const { parse } = require("@webassemblyjs/wast-parser");
+const { print } = require("@webassemblyjs/wast-printer");
+const { readFileSync } = require("fs");
+
+const filename = process.argv[2];
+
+if (typeof filename === "undefined") {
+  throw new Error("Missing file");
+}
+
+const content = readFileSync(filename, "utf8");
+const ast = parse(content);
+
+denormalizeTypeReferences.transform(ast);
+wastIdentifierToIndex.transform(ast);
+
+console.log(print(ast));
